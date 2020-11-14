be80746019a470bfff31511801465eb92bace0c8
masaka13 <masaka724.13@gmail.com>
2020-11-14 21:26:48 +0900

    3-2.stateの変更

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,11 +4,17 @@ const TodoList = () => {
   // 入力されたテキストを管理
   const [input, setInput] = useState('');
   // Todoリストを管理
-  const [todoItems, setTodoItems] = useState([]);
+  const [todoItems, setTodoItems] = useState<string[]>([]);
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
 
   return (
     <>
       <div className="list-container" />
+      <div>入力値：{input}</div>
+      <input type="text" value={input} onChange={handleInput} />
       <button onClick={() => alert('Todoを追加する')}>Todo追加</button>
     </>
   );
