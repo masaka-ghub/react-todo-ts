ad316451571f591d9c22b084adf0e5e35e69aeee
masaka13 <masaka724.13@gmail.com>
2020-11-15 16:02:59 +0900

    5.useEffectでTodo件数を表示する

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
-import TodoItem from './TodoItem';
+import React, { useEffect, useState } from "react";
+import TodoItem from "./TodoItem";
 
 const TodoList = () => {
   // 入力されたテキストを管理
-  const [input, setInput] = useState('');
+  const [input, setInput] = useState("");
   // Todoリストを管理
   const [todoItems, setTodoItems] = useState<string[]>([]);
+  // 件数表示のメッセージ
+  const [message, setMessage] = useState("初期メッセージ");
+
+  useEffect(() => {
+    setMessage(`TODO LIST: ${todoItems.length}件`);
+  }, [todoItems.length]);
 
   const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
@@ -13,11 +19,12 @@ const TodoList = () => {
 
   const addTodo = () => {
     // setterに関数を渡す場合、前回値が引数として使用できる
-    setTodoItems(prev => [...prev, input]);
+    setTodoItems((prev) => [...prev, input]);
   };
 
   return (
     <>
+      <p>{message}</p>
       <div className="list-container">
         {todoItems.map((item, i) => (
           <TodoItem key={i} value={item} />
