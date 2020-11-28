b9c9aaa99a8ff34ea66e9d04d9f0d629e80c9d4a
masaka13 <masaka724.13@gmail.com>
2020-11-28 15:53:11 +0900

    3-3実装例

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -12,18 +12,40 @@ const TodoList = () => {
 
   const addTodo = () => {
     // setterに関数を渡す場合、前回値が引数として使用できる
-    setTodoItems(prev => [...prev, input]);
+    setTodoItems(prev => {
+      if (prev.some(todo => todo === input)) {
+        return prev;
+      }
+      return [...prev, input];
+    });
+
+    // 入力値を削除する
+    setInput('');
   };
+
+  const clearTodo = () => {
+    setTodoItems([]);
+  };
+
+  const removeOwn = () => {};
 
   return (
     <>
       <div className="list-container">
         {todoItems.map((item, i) => (
-          <div key={i}>{item}</div>
+          <div key={i} onClick={removeOwn}>
+            {item}
+          </div>
         ))}
       </div>
       <input type="text" value={input} onChange={handleInput} />
       <button onClick={addTodo}>Todo追加</button>
+      {/* 全削除ボタン */}
+      <div>
+        <button style={{ backgroundColor: '#f06060' }} onClick={clearTodo}>
+          Todo全削除
+        </button>
+      </div>
     </>
   );
 };
